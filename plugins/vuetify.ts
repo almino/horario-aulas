20842a46ca6307ad9c3e3313e5f64c761a5fe11d
Almino Melo <almino.melo@gmail.com>
2025-02-20 18:31:40 -0300

    Definindo a cor padrão e corrigindo o `locale`

--- plugins/vuetify.ts
+++ plugins/vuetify.ts
@@ -6,6 +6,7 @@ import { createVuetify } from "vuetify";
 
 import { md2 } from "vuetify/blueprints";
 import LuxonAdapter from "@date-io/luxon";
+import colors from "vuetify/util/colors";
 // Translations provided by Vuetify
 import { pt } from "vuetify/locale";
 
@@ -18,6 +19,18 @@ export default defineNuxtPlugin((app) => {
     locale: {
       locale: "pt-BR",
       fallback: "pt",
+      messages: { pt },
+    },
+    theme: {
+      themes: {
+        light: {
+          dark: false,
+          colors: {
+            primary: colors.teal.darken1, // #00897B
+            // secondary: colors.red.lighten4, // #FFCDD2
+          },
+        },
+      },
     },
   });
   app.vueApp.use(vuetify);
