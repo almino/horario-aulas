c838c27e05fffe7c01dc6358940f8ea2cbdeae26
Almino Melo <almino.melo@gmail.com>
2025-02-20 15:32:17 -0300

    Seguindo https://vuetifyjs.com/en/getting-started/installation/#using-nuxt-3 `bun add -d vuetify vite-plugin-vuetify bun add @mdi/font`

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,33 @@
+import vuetify, {
+  transformAssetUrls,
+} from "vite-plugin-vuetify";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  build: {
+    transpile: ["vuetify"],
+  },
+  compatibilityDate: "2024-11-01",
+  devtools: { enabled: true },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook(
+        "vite:extendConfig",
+        (config) => {
+          // @ts-expect-error
+          config.plugins.push(
+            vuetify({ autoImport: true })
+          );
+        }
+      );
+    },
+    //...
+  ],
+  vite: {
+    vue: {
+      template: {
+        transformAssetUrls,
+      },
+    },
+  },
+});
